d906693af52bee16e148133c17e65436e0108ccd
Margie Roswell <mroswell@gmail.com>
2014-02-24 21:01:38 -0500

    Created basic table

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+csv_path = './static/la-riots-deaths.csv'
+csv_obj = csv.DictReader(open(csv_path, 'r'))
+csv_list = list(csv_obj)
+
 @app.route("/")
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html',
+        object_list=csv_list,
+    )
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
